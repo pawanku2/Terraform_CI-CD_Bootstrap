b5475d2245fd8faf956f21bd61623d9c333f53e6
kumarpa <kumarpa@Pawans-MacBook-Pro.local>
2023-04-12 21:50:33 +0200

    add vpc

--- main.tf
+++ main.tf
@@ -22,3 +22,29 @@ module "bootstrap" {
   dynamo_db_table_name        = "aws-locks"
 
 }
+
+# Build the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.1.0.0/16"
+  instance_tenancy     = "default"
+  tags = {
+    Name      = "Vpc"
+    Terraform = "true"
+  }
+}
+# Build route table 1
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable1"
+    Terraform = "true"
+  }
+}
+# Build route table 2
+resource "aws_route_table" "route_table2" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "RouteTable2"
+    Terraform = "true"
+  }
+}
